458b7b85bf825224539af8d6ecc26dc064090ab4
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2017-04-12 00:19:01 -0500

    fix exports new version

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import './browser-extends';
-import { init as initParser } from './parser';
+import { init as initParser, parse, doNotParse, allowParse } from './parser';
 import * as component from './component';
+import * as util from './util';
+
+export { bindModelToElement, Create as CreateModel } from './model';
+export { compileExpression } from './expression';
 export { add as controller } from './controller';
+export {
+  component,
+  parse,
+  doNotParse,
+  allowParse,
+  init,
+  util
+};
+
 
 // NOTE this starts all the things
 // init that can be called for vanilla
 function init() {
   initParser(document.body);
 }
-
-
-export {
-  component,
-  init
-};
